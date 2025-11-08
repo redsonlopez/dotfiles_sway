f8741680cf7e9ab7d1db685d6dd6b64b5140a1b2
redsonlopez <redsonlopez@gmail.com>
2025-11-07 23:51:20 -0300

    update

--- qutebrowser/config.py
+++ qutebrowser/config.py
@@ -1,7 +1,11 @@
 config.load_autoconfig(False)
 
-c.statusbar.show = 'in-mode'
+# Interface mínima e leve
+c.statusbar.show = 'in-mode'      # Mostra barra de status apenas fora do modo normal
+c.tabs.show = 'multiple'          # Mostra abas apenas quando há mais de uma
+c.scrolling.smooth = False        # Desativa rolagem suave (menos uso de CPU)
 
+# Flags do QtWebEngine para performance e compatibilidade
 c.qt.args = [
     "--disable-gpu",
     "--disable-gpu-compositing",
@@ -17,10 +21,19 @@ c.qt.args = [
     "--disable-features=VizDisplayCompositor,UseSkiaRenderer,CanvasOopRasterization"
 ]
 
-c.content.cache.size = 50 * 1024 * 1024
-c.content.javascript.enabled = True
-config.set('content.javascript.enabled', True, 'https://www.youtube.com/')
+# Uso moderado de cache e histórico
+c.content.cache.size = 50 * 1024 * 1024  # 50 MB
 c.auto_save.session = False
-c.history_gap_interval = 3600
+c.history_gap_interval = 3600            # Salva histórico a cada 1h
+
+# Bloqueio de anúncios (baixe listas com :adblock-update)
+c.content.blocking.enabled = True
+c.content.blocking.method = "auto"
+
+# Imagens habilitadas
 c.content.images = True
+
+# Desativar avisos de autoplay e vídeos em tela cheia
+c.content.autoplay = True
+c.content.fullscreen.window = True
 
