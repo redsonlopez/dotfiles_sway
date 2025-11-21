28b5510af6b848480acd8fa7a520fd4ebcc48116
redsonlopez <redsonlopez@gmail.com>
2025-11-21 12:34:26 -0300

    update nvim

--- nvim/lua/red/settings.lua
+++ nvim/lua/red/settings.lua
@@ -1,22 +1,36 @@
 local o = vim.opt
 
-o.relativenumber = true
-o.tabstop = 2
-o.shiftwidth = 2
-o.expandtab = true
-o.termguicolors = true
-o.cursorline = true
-
 -- Clipboard
 o.clipboard = { "unnamed", "unnamedplus" }
 
--- Tabline
+-- Cores bem aceitas independente do terminal
+o.termguicolors = true
+
+-- Tabline (Tittle)
 o.showtabline = 2
 o.tabline = "%#TabLineSel# "
 
+-- Identação e tabulações
+o.autoindent = true
+o.smartindent = true
+o.expandtab = true
+o.tabstop = 2
+o.shiftwidth = 2
+o.softtabstop = 2
+
+-- Exibir caracteres de tabulação e espaços em branco
+--o.list = true
+--o.listchars = "tab:»·,trail:·"
+
 -- Number + Fold
+o.relativenumber = true
+o.cursorline = true
 o.numberwidth = 4
 o.foldcolumn = "2"
+
+-- Definir largura máxima da linha
+--o.textwidth = 79
+--o.formatoptions = o.formatoptions + "t"
 
 -- Remover tildes do final do arquivo
 o.fillchars = { fold = " ", eob = " " }
