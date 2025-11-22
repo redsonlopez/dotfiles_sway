8c343e9e2e83cdcef43b13529e8dfbd806b02676
redsonlopez <redsonlopez@gmail.com>
2025-11-22 11:45:39 -0300

    add NvimTree in keymaps

--- nvim/lua/red/keymaps.lua
+++ nvim/lua/red/keymaps.lua
@@ -2,6 +2,12 @@ vim.g.mapleader = " "
 
 local map = vim.keymap.set
 
+-- Abre/fecha NvimTree com Space+e 
 map("n", "<leader>e", ":NvimTreeToggle<CR>", { silent = true })
+-- Sai do modo insert com jj
 map("i", "jj", "<Esc>")
+-- Próximo buffer com Shift+l
+map("n", "<S-l>", ":bnext<CR>", { silent = true })
+-- Buffer anterior com Shift+h
+map("n", "<S-h>", ":bprevious<CR>", { silent = true })
 
